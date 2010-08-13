2b06c37e088d06d8f7820af69384f48895bfb11c
Alex Clark <aclark@aclark.net>
2010-08-13 12:01:22 +0000

    Make it all work

--- buildout/bootstrap/__init__.py
+++ buildout/bootstrap/__init__.py
@@ -1,40 +1,28 @@
 import urllib
-import zc.buildout.easy_install
 
-logger = zc.buildout.easy_install.logger
 
 def install(buildout):
     """
-    You know, I have one simple request. And that is to have sharks with 
-    frickin' laser beams attached to their heads! 
-    
+    You know, I have one simple request. And that is to have sharks with
+    frickin' laser beams attached to their heads!
+
     Actually, all I really want is to avoid typing:
 
-        $ wget http://svn.zope.org/repos/main/zc.buildout/trunk/bootstrap/bootstrap.py
+        $ wget http://svn.zope.org/repos/main/zc.buildout/trunk/bootstrap/\
+            bootstrap.py
 
     """
-
-    url = 'http://svn.zope.org/repos/main/zc.buildout/trunk/bootstrap/bootstrap.py'
+    base = 'http://svn.zope.org'
+    url = base + '/repos/main/zc.buildout/trunk/bootstrap/bootstrap.py'
     data = urllib.urlopen(url).read()
-    file = '/'.join([self.buildout['buildout']['directory'],'bootstrap.py'])
-    bootstrap_py = open(file,'w')
-    bootstrap_py.write(data)
-    bootstrap_py.close()
+    file = '/'.join([buildout['buildout']['directory'], 'bootstrap.py'])
+    bootstrap = open(file, 'w')
+    bootstrap.write(data)
+    bootstrap.close()
 
-#    file_name = 'dump-picked-versions-file' in buildout['bootstrap'] and \
-#                buildout['buildout']['dump-picked-versions-file'].strip() or \
-#                None
-#                
-#    overwrite = 'overwrite-picked-versions-file' not in buildout['buildout'] or \
-#                buildout['buildout']['overwrite-picked-versions-file'].lower() \
-#                in ['yes', 'true', 'on']
-#
-#    zc.buildout.easy_install.Installer.__picked_versions = {}
-#    zc.buildout.easy_install._log_requirement = _log_requirement
-#    zc.buildout.easy_install.Installer._get_dist = enable_dumping_picked_versions(
-#                                  zc.buildout.easy_install.Installer._get_dist)
-#    
-#    logging.shutdown = dump_picked_versions(logging.shutdown, 
-#                                            file_name, 
-#                                            overwrite)
-#
+    print ('----------------------------------------' +
+            '----------------------------------------')
+    print 'Downloading zc.buildout \'bootstrap.py\' file from:\n  %s' % url
+    print 'To:\n  %s' % file
+    print ('----------------------------------------' +
+            '----------------------------------------')
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup (
-    name = 'buildout.bootstrap'
+    name = 'buildout.bootstrap',
+    packages = find_packages(),
+    install_requires = ['setuptools'],
+    namespace_packages = ['buildout'],
+    entry_points = {"zc.buildout.extension": ["default = buildout.bootstrap:install"]}
 )

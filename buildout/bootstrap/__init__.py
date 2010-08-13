98e55d308f9d9284044c0b56ce86074dee9fae81
Alex Clark <aclark@aclark.net>
2010-08-13 10:20:40 +0000

    Start to bootstrap writing the bootstrap

--- buildout/bootstrap/__init__.py
+++ buildout/bootstrap/__init__.py
@@ -1,31 +1,40 @@
-# -*- coding: utf-8 -*-
-"""Recipe bootstrap"""
+import urllib
+import zc.buildout.easy_install
 
-class Recipe(object):
-    """zc.buildout recipe"""
+logger = zc.buildout.easy_install.logger
 
-    def __init__(self, buildout, name, options):
-        self.buildout, self.name, self.options = buildout, name, options
+def install(buildout):
+    """
+    You know, I have one simple request. And that is to have sharks with 
+    frickin' laser beams attached to their heads! 
+    
+    Actually, all I really want is to avoid typing:
 
-    def install(self):
-        """Installer"""
-        # XXX Implement recipe functionality here
+        $ wget http://svn.zope.org/repos/main/zc.buildout/trunk/bootstrap/bootstrap.py
 
-        # You know, I have one simple request. And that is to have sharks with 
-        # frickin' laser beams attached to their heads! Actually all I really what to do is avoid:
-        # svn cat svn://svn.zope.org/repos/main/zc.buildout/trunk/bootstrap/bootstrap.py > bootstrap.py
-        import urllib
-        url = 'http://svn.zope.org/repos/main/zc.buildout/trunk/bootstrap/bootstrap.py'
-        data = urllib.urlopen(url).read()
-        file = '/'.join([self.buildout['buildout']['directory'],'bootstrap.py'])
-        bootstrap_py = open(file,'w')
-        bootstrap_py.write(data)
-        bootstrap_py.close()
+    """
 
-        # Return files that were created by the recipe. The buildout
-        # will remove all returned files upon reinstall.
-        return tuple()
+    url = 'http://svn.zope.org/repos/main/zc.buildout/trunk/bootstrap/bootstrap.py'
+    data = urllib.urlopen(url).read()
+    file = '/'.join([self.buildout['buildout']['directory'],'bootstrap.py'])
+    bootstrap_py = open(file,'w')
+    bootstrap_py.write(data)
+    bootstrap_py.close()
 
-    def update(self):
-        """Updater"""
-        pass
+#    file_name = 'dump-picked-versions-file' in buildout['bootstrap'] and \
+#                buildout['buildout']['dump-picked-versions-file'].strip() or \
+#                None
+#                
+#    overwrite = 'overwrite-picked-versions-file' not in buildout['buildout'] or \
+#                buildout['buildout']['overwrite-picked-versions-file'].lower() \
+#                in ['yes', 'true', 'on']
+#
+#    zc.buildout.easy_install.Installer.__picked_versions = {}
+#    zc.buildout.easy_install._log_requirement = _log_requirement
+#    zc.buildout.easy_install.Installer._get_dist = enable_dumping_picked_versions(
+#                                  zc.buildout.easy_install.Installer._get_dist)
+#    
+#    logging.shutdown = dump_picked_versions(logging.shutdown, 
+#                                            file_name, 
+#                                            overwrite)
+#

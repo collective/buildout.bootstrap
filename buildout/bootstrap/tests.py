ab0ecca36d721516b48a7bc9aaf2340c0d9656c5
Alex Clark <aclark@aclark.net>
2010-08-14 02:24:39 +0000

    Check in stubs

---
+++ buildout/bootstrap/tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestBuildoutBootstrap(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_foo(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
